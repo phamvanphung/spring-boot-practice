9ee01fdf9d6f3bc4d6d5a41152215fa2251043c1
phungpv <phungpv@sunshinegroup.vn>
2023-09-14 17:33:04 +0700

    add base spring sercurity jwt

---
+++ spring-boot-sercurity-jwt/src/main/java/com/example/demo/SpringBootSercurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSercurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSercurityJwtApplication.class, args);
+    }
+
+}
